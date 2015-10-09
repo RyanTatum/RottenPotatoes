0bb69919e55df9701ac501e470331f5b33a2e29e
ryantatum <ryant9211@gmail.com>
2015-10-09 03:41:45 +0000

    now that database should be setup correctly

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+    
+    def session_params
+        params.require(:user).permit(:user_id, :email, :session_token)
+    end
+  
+    def new
+        # default, display new.html
+    end
+    
+    def create
+        user = User.find_by_user_id(session_params[:user_id])
+
+        if !user || user.email != session_params[:email]
+            flash[:notice] = "Invalid User-ID/E-Mail combination"
+            redirect_to login_path
+        else
+            session[:session_token] = user.session_token
+            redirect_to movies_path
+        end
+    end
+    
+    def destroy
+        reset_session
+        redirect_to movies_path
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def user_params
+    params.require(:user).permit(:user_id, :email, :session_token)
+  end
+  
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    if User.find_by_user_id user_params[:user_id]
+       flash[:notice] = "Sorry, this user-id is taken. Try again." 
+       redirect_to new_user_path
+    else
+        flash[:notice] = "Created new user #{user_params[:user_id]}."
+        @user = User.create_user!(user_params)
+        redirect_to login_path
+    end
+  end
+end
